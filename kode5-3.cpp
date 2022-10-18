cbb045388678af9a19a2f1845c6ce1ea91eef159
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2022-10-18 13:33:30 +0700

    Add files via upload

---
+++ kode5-3.cpp
@@ -0,0 +1,21 @@
+/**********************************************************
+*  PEMROGRAMAN C++                                        *
+*                                                         *
+*  Author  : Gesa Rizky Nugraha                           *
+*  Email   : gesarizkynugraha@gmail.com                   *
+*                                                         *
+*  Website : karenabelajar.blogspotcom                    *
+*                                                         *
+***********************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int C;
+  for (C=10; C>0; C--) {
+     cout<<"Bisa Karena Belajar"<<endl;
+  }
+
+  return 0;
+}
